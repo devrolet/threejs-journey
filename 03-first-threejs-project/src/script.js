fe1fd0d5bf76998a3928dc4c5e3bac1d5b9e58bc
Chet Hill <chetdhill@gmail.com>
2024-05-08 16:57:56 -0500

    Create a full Three.js scene

--- 03-first-threejs-project/src/script.js
+++ 03-first-threejs-project/src/script.js
@@ -1,2 +1,31 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene 
+const scene = new THREE.Scene();
+
+// Geometry + Material = Three.js Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+// Add object to scene
+scene.add(mesh);
+
+const sizes = {
+    width: 800, 
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
