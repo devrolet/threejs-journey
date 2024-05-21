5fb7ae694578e76578f4df6c4115e4f10e83dd6a
Chet Hill <chetdhill@gmail.com>
2024-05-21 16:23:16 -0500

    Three.js Mesh Material

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -11,6 +11,60 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg');
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg');
+const matcapTexture = textureLoader.load('./textures/matcaps/1.png');
+const gradientTexture = textureLoader.load('./textures/gradients/3.jpg');
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Objects
+ */
+// Mesh Basic Material
+// const material = new THREE.MeshBasicMaterial();
+// material.map = doorColorTexture
+// material.color = new THREE.Color('#f00');
+// material.wireframe = true
+// material.transparent = true
+// must set transparent to true to see the opacity
+// material.opacity = 0.2;
+// material.alphaMap = doorAlphaTexture
+// TODO: Sides can be FrontSide, BackSide, and DoubleSide
+// material.side = THREE.DoubleSide
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5;
+
+
+
+scene.add(sphere, plane, torus);
+
+/**
  * Sizes
  */
 const sizes = {
@@ -64,6 +118,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime;
+    plane.rotation.y = 0.1 * elapsedTime;
+    torus.rotation.y = 0.1 * elapsedTime;
+
+    sphere.rotation.x = -0.15 * elapsedTime;
+    plane.rotation.x = -0.15 * elapsedTime;
+    torus.rotation.x = -0.15 * elapsedTime;
 
     // Update controls
     controls.update()
