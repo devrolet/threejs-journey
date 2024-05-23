d516cecab96d1607aba17a1de710d290cbd37d25
Chet Hill <chetdhill@gmail.com>
2024-05-23 12:20:34 -0500

    Create 3D Text

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 /**
  * Base
  */
@@ -20,14 +21,32 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 /**
- * Object
+ * Fonts
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-
-scene.add(cube)
+const fontLoader = new FontLoader();
+console.log('Font Loader: ', fontLoader);
+fontLoader.load(
+    'fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Brave New World',
+            {
+                font: font,
+                size: 0.5,
+                depth: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const textMaterial = new THREE.MeshBasicMaterial();
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text);
+    }
+);
 
 /**
  * Sizes
