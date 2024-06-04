723fa1b68de90b4bda77e57e159a7d79c551f012
Chet Hill <chetdhill@gmail.com>
2024-06-04 09:06:23 -0500

    Go Live Lesson

--- 13-go-live/src/script.js
+++ 13-go-live/src/script.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import GUI from 'lil-gui'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
 /**
  * Base
  */
@@ -15,71 +16,63 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// // Axes helper
-// const axisHelper = new THREE.AxesHelper();
-// scene.add(axisHelper);
-
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load('/textures/matcaps/5.png');
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('textures/matcaps/8.png')
 matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 /**
  * Fonts
  */
-const fontLoader = new FontLoader();
+const fontLoader = new FontLoader()
+
 fontLoader.load(
-    'fonts/helvetiker_regular.typeface.json',
-    (font) => {
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        // Material
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+        // Text
         const textGeometry = new TextGeometry(
-            'Brave New World',
+            'Hello Three.js',
             {
                 font: font,
                 size: 0.5,
                 depth: 0.2,
-                curveSegments: 5,
+                curveSegments: 12,
                 bevelEnabled: true,
                 bevelThickness: 0.03,
                 bevelSize: 0.02,
                 bevelOffset: 0,
-                bevelSegments: 4
+                bevelSegments: 5
             }
         )
-        textGeometry.computeBoundingBox();
+        textGeometry.center()
 
-        // TODO: Centers 3D Text
-        // textGeometry.translate(
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-        // )
-        textGeometry.center();
-        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
         const text = new THREE.Mesh(textGeometry, material)
-        scene.add(text);
+        scene.add(text)
 
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-        
+        // Donuts
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
 
-        for(let i = 0; i < 200; i++) {
-            const donut = new THREE.Mesh(donutGeometry, material);
+        for(let i = 0; i < 100; i++)
+        {
+            const donut = new THREE.Mesh(donutGeometry, material)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
 
-            donut.position.x = (Math.random() - 0.5) * 10;
-            donut.position.y = (Math.random() - 0.5) * 10;
-            donut.position.z = (Math.random() - 0.5) * 10;
-
-            donut.rotation.x = Math.random() * Math.PI;
-            donut.rotation.y = Math.random() * Math.PI;
-
-            const scale = Math.random();
-            donut.scale.set(scale, scale, scale);
-
-            scene.add(donut);
+            scene.add(donut)
         }
     }
-);
+)
 
 /**
  * Sizes
