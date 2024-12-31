282809c3fb07c688c12d3b2d82486847522a9f46
Chet Hill <chetdhill@gmail.com>
2024-12-31 13:48:58 -0600

    Three.js Shadows: Alternate Baked Shadow with Animation on Shadow

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -9,7 +9,8 @@ const textureLoader = new THREE.TextureLoader();
 const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
 bakedShadow.colorSpace = THREE.SRGBColorSpace;
 
-console.log('Baked Shadow File: ', bakedShadow);
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg');
+// simpleShadow.colorSpace = THREE.SRGBColorSpace;
 
 
 /**
@@ -125,9 +126,7 @@ sphere.castShadow = true;
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    new THREE.MeshBasicMaterial({
-        map: bakedShadow
-    })
+    material
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
@@ -135,6 +134,20 @@ plane.position.y = - 0.5
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphereShadow, plane);
 
 /**
  * Sizes
@@ -198,6 +211,17 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update the sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    // Update the shadow
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3;
 
     // Update controls
     controls.update()
