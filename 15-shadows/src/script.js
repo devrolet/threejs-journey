d5ceb3ccef52f40076d283a782a8cf52aed23b22
Chet Hill <chetdhill@gmail.com>
2024-12-31 13:10:39 -0600

    Three.js Shadows: Point Light

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -79,6 +79,22 @@ const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 spotLightCameraHelper.visible = false;
 scene.add(spotLightCameraHelper)
 
+// Point Light
+const pointLight = new THREE.PointLight(0xffffff, 2.7);
+pointLight.castShadow = true;
+
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 2
+
+pointLight.position.set(-1, 1, 0);
+scene.add(pointLight);
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+// pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
+
 /**
  * Materials
  */
