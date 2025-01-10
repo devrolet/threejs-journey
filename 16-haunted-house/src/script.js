c5310d70c7dfadcd7df99ccfe11e3f26bb9f4b2e
Chet Hill <chetdhill@gmail.com>
2025-01-09 18:14:31 -0600

    ALERT: THIS IS AN EXAMPLE OF Z-FIGHTING THE GPU IS CONFUSED WHAT SHOULD BE FIRST

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -54,6 +54,17 @@ roof.position.y = 2.5 + 0.75;
 roof.rotation.y = Math.PI * 0.25
 house.add(roof);
 
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial({
+        color: 'red'
+    })
+)
+door.position.y = 1;
+door.position.z = 2;
+house.add(door);
+
 /**
  * Lights
  */
